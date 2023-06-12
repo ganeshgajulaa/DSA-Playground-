e91b86e5412f3a24008a9469cdde47d851fd3945
Ganesh <96542244+ganeshgajulaa@users.noreply.github.com>
2023-06-12 13:39:05 +0530

    Time: 3 ms (93.67%), Space: 42.1 MB (92.08%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,12 +1,17 @@
 class Solution {
     public int numOfPairs(String[] nums, String k) {
+        Map<String,Integer> mp = new HashMap<>();
         int cnt = 0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(nums[i].concat(nums[j]).equals(k) && i!=j){
-                    cnt++;
-                }
+        for(String num:nums){
+            if(k.startsWith(num)){
+                String suffix = k.substring(num.length());
+                cnt+=mp.getOrDefault(suffix,0);
             }
+            if(k.endsWith(num)){
+                String prefix = k.substring(0,k.length() - num.length());
+                cnt+=mp.getOrDefault(prefix,0);
+            }
+            mp.put(num,mp.getOrDefault(num,0)+1);
         }
         return cnt;
     }
